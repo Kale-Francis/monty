c00f3dc31ddb7c93e41433ae1187de7c2e42d965
king-vader <kalefrancis989@gmail.com>
2024-08-26 19:16:32 +0300

    my first monty

---
+++ README.md
@@ -0,0 +1,2 @@
+c monty
+updated project
---
+++ monty.c
@@ -0,0 +1,116 @@
+#include "monty.h"
+
+#define BUFFER_SIZE 1024
+
+/**
+ * push - Pushes an element to the stack.
+ * @stack: Double pointer to the top of the stack.
+ * @line_number: Line number where the instruction appears.
+ * @arg: The argument to be pushed onto the stack.
+ */
+void push(stack_t **stack, unsigned int line_number, char *arg)
+{
+    int num;
+    stack_t *new_node;
+
+    if (arg == NULL || ((num = atoi(arg)) == 0 && strcmp(arg, "0") != 0))
+    {
+        fprintf(stderr, "L%u: usage: push integer\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    new_node = malloc(sizeof(stack_t));
+    if (new_node == NULL)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    new_node->n = num;
+    new_node->prev = NULL;
+    new_node->next = *stack;
+
+    if (*stack != NULL)
+        (*stack)->prev = new_node;
+
+    *stack = new_node;
+}
+
+/**
+ * pall - Prints all the values on the stack, starting from the top.
+ * @stack: Double pointer to the top of the stack.
+ * @line_number: Line number where the instruction appears (unused).
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+    stack_t *current = *stack;
+
+    (void)line_number; /* Avoid unused parameter warning */
+
+    while (current != NULL)
+    {
+        printf("%d\n", current->n);
+        current = current->next;
+    }
+}
+
+/**
+ * main - Entry point for the Monty bytecode interpreter.
+ * @argc: Argument count.
+ * @argv: Argument vector.
+ *
+ * Return: EXIT_SUCCESS on success, or EXIT_FAILURE on failure.
+ */
+int main(int argc, char *argv[])
+{
+    FILE *file;
+    char line[BUFFER_SIZE];
+    unsigned int line_number = 0;
+    stack_t *stack = NULL;
+    char *opcode;  /* Moved declarations to the start of the block */
+    char *arg;
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    file = fopen(argv[1], "r");
+    if (file == NULL)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    while (fgets(line, BUFFER_SIZE, file) != NULL)
+    {
+        line_number++;
+        opcode = strtok(line, " \n");
+        arg = strtok(NULL, " \n");
+
+        if (opcode == NULL || opcode[0] == '#')
+            continue;
+
+        if (strcmp(opcode, "push") == 0)
+            push(&stack, line_number, arg);
+        else if (strcmp(opcode, "pall") == 0)
+            pall(&stack, line_number);
+        else
+        {
+            fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    fclose(file);
+
+    while (stack != NULL)
+    {
+        stack_t *temp = stack;
+        stack = stack->next;
+        free(temp);
+    }
+
+    return EXIT_SUCCESS;
+}
